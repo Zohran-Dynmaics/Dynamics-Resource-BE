30021d63590a07b8ff4ced143d36ba20fbc6da9e
Zohran-Dynamics <zohran.rasheed@dynamicsplus.ae>
2024-07-25 10:46:29 +0500

    refactor code in global contant file

--- src/shared/constant.ts
+++ src/shared/constant.ts
@@ -4,81 +4,66 @@ import { getDayBoundaries } from "./utility/utility";
 export const HASH_SALT = 10;
 export const CRM_VERSION = "v9.1";
 const COMMON_URL = `api/data/${CRM_VERSION}`;
+const END_POINTS = {
+    BOOKABALE_RESOURCE_BOOKINGS: `${COMMON_URL}/bookableresourcebookings?`,
+}
 
 const DATES = {
-    today: (date: Date) => getDayBoundaries(
-        new Date(date).toISOString(),
-    ),
+    today: (date: Date) => getDayBoundaries(new Date(date).toISOString()),
     tomorrow: (date: Date) => {
-        const tomorrow = new Date();
+        const tomorrow = new Date(date);
         tomorrow.setDate(date.getDate() + 7);
-        return getDayBoundaries(
-            tomorrow
-        )
+        return getDayBoundaries(tomorrow);
     },
     week: (date: Date) => {
-        const { startOfDay } = getDayBoundaries(new Date(date).toISOString())
-        const nextWeekDate = new Date();
+        const { startOfDay } = getDayBoundaries(new Date(date).toISOString());
+        const nextWeekDate = new Date(date);
         nextWeekDate.setDate(date.getDate() + 7);
         const { endOfDay } = getDayBoundaries(nextWeekDate);
 
         return { startOfDay, endOfDay };
     }
+};
 
-}
+const buildQueryParams = (query: any | null, resourceId: string, date?: Date | string | null) => {
+    const params = {
+        '$expand': 'msdyn_workorder($expand=msdyn_workordertype($select=msdyn_name),msdyn_FunctionalLocation($select=msdyn_name),msdyn_serviceaccount($select=name)),BookingStatus($select=name)',
+        '$filter': `_resource_value eq ${resourceId}`,
+        '$count': 'true'
+    };
+
+    if (query?.filter) {
+        const { startOfDay, endOfDay } = DATES[query?.filter](date ? new Date(date) : new Date());
+        params['$filter'] += ` and starttime ge ${startOfDay} and starttime lt ${endOfDay}`;
+    }
+
+    if (query?.workordertype) {
+        params['$filter'] += ` and msdyn_workorder/_msdyn_workordertype_value eq ${query?.workordertype}`;
+    }
+
+    if (date) {
+        const { startOfDay, endOfDay } = getDayBoundaries(
+            new Date(date).toISOString(),
+        );
+        params['$filter'] += ` and starttime ge ${startOfDay} and starttime lt ${endOfDay}`
+    }
+
+    return Object.entries(params)
+        .map(([key, value]) => `${key}=${value}`)
+        .join('&');
+};
 
 export const URLS_AND_QUERY_PARAMS = {
     BOOKING: {
         GET: {
-            TASKS: {
-                endpoint: (base_url: string): string =>
-                    `${base_url}${COMMON_URL}/bookableresourcebookings?`,
-
-                query: (query: any | null, resourceId: string) => {
-                    const { filter = null, workordertype = null } = query;
-                    const params = {
-                        // '$select': 'starttime,endtime,duration',
-                        '$expand': 'msdyn_workorder($expand=msdyn_workordertype($select=msdyn_name),msdyn_FunctionalLocation($select=msdyn_name),msdyn_serviceaccount($select=name)),BookingStatus($select=name)',
-                        '$filter': `_resource_value eq ${resourceId}`,
-                        '$count': 'true'
-                    };
-                    if (filter) {
-                        const date = new Date();
-                        const { startOfDay, endOfDay } = DATES[filter](date);
-                        params['$filter'] += ` and starttime ge ${startOfDay} and starttime lt ${endOfDay}`
-                    }
-                    if (workordertype) {
-                        params['$filter'] += ` and msdyn_workorder/_msdyn_workordertype_value eq ${workordertype}`
-                    }
-                    return Object.entries(params)
-                        .map(([key, value]) => `${key}=${value}`)
-                        .join('&');
-                }
+            BOOKINGS: {
+                endpoint: (base_url: string): string => `${base_url}${END_POINTS.BOOKABALE_RESOURCE_BOOKINGS}`,
+                query: (query: any | null, resourceId: string) => buildQueryParams(query, resourceId)
             },
-            TASKS_FOR_CALENDER: {
-                endpoint: (base_url: string): string =>
-                    `${base_url}${COMMON_URL}/bookableresourcebookings?`,
-
-                query: (date: Date | string | null, resourceId: string) => {
-                    const params = {
-                        // '$select': 'starttime,endtime,duration',
-                        '$expand': 'msdyn_workorder($expand=msdyn_workordertype($select=msdyn_name),msdyn_FunctionalLocation($select=msdyn_name),msdyn_serviceaccount($select=name)),BookingStatus($select=name)',
-                        '$filter': `_resource_value eq ${resourceId}`,
-                        '$count': 'true'
-                    };
-                    if (date) {
-                        const { startOfDay, endOfDay } = getDayBoundaries(
-                            new Date(date).toISOString(),
-                        );
-                        params['$filter'] = `starttime ge ${startOfDay} and starttime lt ${endOfDay} and _resource_value eq ${resourceId}`
-                    }
-                    return Object.entries(params)
-                        .map(([key, value]) => `${key}=${value}`)
-                        .join('&');
-                }
+            BOOKINGS_FOR_CALENDER: {
+                endpoint: (base_url: string): string => `${base_url}${END_POINTS.BOOKABALE_RESOURCE_BOOKINGS}`,
+                query: (date: Date | string | null, resourceId: string) => buildQueryParams(null, resourceId, date)
             },
-
         }
     }
-
 };
