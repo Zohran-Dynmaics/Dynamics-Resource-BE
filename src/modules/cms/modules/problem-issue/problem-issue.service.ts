4ba588e2aa98eccfd5784190703b9271cca535b4
Zohran-Dynamics <zohran.rasheed@dynamicsplus.ae>
2024-07-31 11:41:29 +0500

    all problem issue api created

--- src/modules/cms/modules/problem-issue/problem-issue.service.ts
+++ src/modules/cms/modules/problem-issue/problem-issue.service.ts
@@ -2,21 +2,23 @@ import { Injectable } from "@nestjs/common";
 import { AxiosRequestConfig } from "axios";
 import { ApiService } from "src/modules/api/api.service";
 import { HTTPS_METHODS } from "src/shared/enum";
+import { PROBLEM_ISSUES_ENDPOINTS } from "./problem-issue.contant";
 
 @Injectable()
 export class ProblemIssueService {
-  constructor(private apiService: ApiService) {}
+  constructor(private apiService: ApiService) { }
 
   async getAllProblemIssues(
     token: string,
     base_url: string,
     query?: any,
   ): Promise<any> {
+    const { endpoint, searchQuery } = PROBLEM_ISSUES_ENDPOINTS.ALL_PROBLEM_ISSUES;
     const config: AxiosRequestConfig = this.apiService.getConfig(
-      `${base_url}api/data/v9.1/cafm_problemissues`,
+      `${endpoint(base_url)}`,
       HTTPS_METHODS.GET,
       token,
-      query,
+      searchQuery(query) as string,
     );
     try {
       return await this.apiService.request(config);
